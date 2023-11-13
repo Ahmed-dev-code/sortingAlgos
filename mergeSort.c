fc1864809dafebea99a234c401a42e86eade4e85
Ahmed Belloula <belloulaahmed7@gmail.com>
2023-11-13 22:39:28 +0100

    optimised memory consumption

--- mergeSort.c
+++ mergeSort.c
@@ -32,86 +32,75 @@ void fusion(int tab1[], int n1, int tab2[], int n2, int T[]) {
 
 void triFusion(int T[], int n) {
     if (n > 1) {
-        int i;
         int n1 = n / 2;
         int n2 = n - n1;
 
-        int tab1[n1];
-        int tab2[n2];
+        // Reuse T for tab1 and tab2
+        int *tab1 = T;
+        int *tab2 = T + n1;
 
-        // Initialiser et copier la première moitié du tableau dans tab1
-        for (i = 0; i < n1; i++) {
-            tab1[i] = T[i];
-        }
-
-        // Initialiser et copier la deuxième moitié du tableau dans tab2
-        for (i = 0; i < n2; i++) {
-            tab2[i] = T[i + n1];
-        }
-
-        // Appeler récursivement triFusion pour chaque moitié
         triFusion(tab1, n1);
         triFusion(tab2, n2);
 
-        // Appeler la fonction de fusion
-        fusion(tab1, n1, tab2, n2, T);
+        int *temp = (int *)malloc(n * sizeof(int));
+        // Check if malloc was successful, handle error if not
+
+        fusion(tab1, n1, tab2, n2, temp);
+
+        // Copy the merged result back to T
+        int i;
+        for (i = 0; i < n; i++) {
+            T[i] = temp[i];
+        }
+
+        free(temp);
     }
 }
 
-
-void generateTable(int n, int **ordered, int **inverted, int **random)
-{
-    // Allocate memory for tables
-    *ordered = (int *)malloc(n * sizeof(int));
-    *inverted = (int *)malloc(n * sizeof(int));
-    *random = (int *)malloc(n * sizeof(int));
+void generateRandomTable(int n, int **table) {
+    *table = (int *)malloc(n * sizeof(int));
     int i;
-    // Generate ordered table
-    for (i = 0; i < n; i++)
-    {
-        (*ordered)[i] = i + 1;
+    for (i = 0; i < n; i++) {
+        (*table)[i] = i + 1;
     }
 
-    // Generate inverted table
-    for (i = 0; i < n; i++)
-    {
-        (*inverted)[i] = n - i;
-    }
-
-    // Generate randomly shuffled table
-    for (i = 0; i < n; i++)
-    {
-        (*random)[i] = i + 1;
-    }
-
-    // Shuffle the randomly shuffled table using the Fisher-Yates algorithm
+    // Shuffle the table using the Fisher-Yates algorithm
     srand((unsigned int)time(NULL));
-    for (i = n - 1; i > 0; i--)
-    {
+    for (i = n - 1; i > 0; i--) {
         int j = rand() % (i + 1);
         // Swap
-        int temp = (*random)[i];
-        (*random)[i] = (*random)[j];
-        (*random)[j] = temp;
+        int temp = (*table)[i];
+        (*table)[i] = (*table)[j];
+        (*table)[j] = temp;
     }
 }
 
-void printTable(int T[], int n)
-{
+void generateTable(int n, int **table) {
+    *table = (int *)malloc(n * sizeof(int));
     int i;
-    for (i = 0; i < n; i++)
-    {
+    for (i = 0; i < n; i++) {
+        (*table)[i] = i + 1;
+    }
+}
+
+void generateInverseTable(int n, int **table) {
+    *table = (int *)malloc(n * sizeof(int));
+    int i;
+    for (i = 0; i < n; i++) {
+        (*table)[i] = n - i;
+    }
+}
+
+void printTable(int T[], int n) {
+    int i;
+    for (i = 0; i < n; i++) {
         printf("%d ", T[i]);
     }
     printf("\n");
 }
 
-
-
-
 int main(int argc, char *argv[]) {
-    if (argc != 2)
-    {
+    if (argc != 2) {
         printf("Usage: %s <n>\n", argv[0]);
         return 1; // Exit with an error code
     }
@@ -119,70 +108,58 @@ int main(int argc, char *argv[]) {
     int n = atoi(argv[1]); // Convert the second command line argument to an integer
     printf("n: %d\n", n);
 
-    int *ordered, *inverted, *random;
+    int *table;
 
     clock_t start_time = clock();
-    generateTable(n, &ordered, &inverted, &random);
+    generateTable(n, &table);
     clock_t end_time = clock();
     double elapsed_time_generating = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
     printf("\n");
     printf("Time of execution (generating): %f seconds\n", elapsed_time_generating);
 
-    // // print the table before sorting
-    // printf("Ordered Table (Unsorted): ");
-    // printTable(ordered, n);
-    // printf("Randomly Shuffled Table (Unsorted): ");
-    // printTable(random, n);
-    // printf("Inverted Table (Unsorted): ");
-    // printTable(inverted, n);
-
-    
-
-    // Sort and measure time for the ordered table
+    // Sort and measure time for the table
     start_time = clock();
-    triFusion(ordered, n);
+    triFusion(table, n);
     end_time = clock();
     double elapsed_time_ordered = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
-    printf("Ordered Table (Sorted): ");
+    printf("Table Ordered (Sorted): ");
     printf("\n");
-    printf("Time of execution (Ordered): %f seconds\n", elapsed_time_ordered);
-    free(ordered);
+    printf("Time of execution: %f seconds\n", elapsed_time_ordered);
+    free(table);
 
-    // Sort and measure time for the randomly shuffled table
+    // Generate a random table and measure time for it
     start_time = clock();
-    triFusion(random, n);
+    generateRandomTable(n, &table);
     end_time = clock();
     double elapsed_time_random = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
-    printf("Randomly Shuffled Table (Sorted): ");
-    printf("\n");
-    printf("Time of execution (Random): %f seconds\n", elapsed_time_random);
-    free(random);
 
-    // Sort and measure time for the inverted table
+    // Sort and measure time for the random table
     start_time = clock();
-    triFusion(inverted, n);
+    triFusion(table, n);
     end_time = clock();
-    double elapsed_time_inverted = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
-    printf("Inverted Table (Sorted): ");
+    double elapsed_time_random_ordered = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
+    printf("Table Random Ordered (Sorted): ");
     printf("\n");
-    printf("Time of execution (Inverted): %f seconds\n", elapsed_time_inverted);
-    free(inverted);
+    printf("Time of execution: %f seconds\n", elapsed_time_random_ordered);
+    free(table);
+    
+    // Generate an inverse table and measure time for it
+    start_time = clock();
+    generateInverseTable(n, &table);
+    end_time = clock();
+    double elapsed_time_inverse = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
 
-    // // Print the sorted tables
-    // printf("Ordered Table (sorted): ");
-    // printTable(ordered, n);
-    // printf("Randomly Shuffled Table (sorted): ");
-    // printTable(random, n);
-    // printf("Inverted Table (sorted): ");
-    // printTable(inverted, n);
-
-
+    // Sort and measure time for the inverse table
+    start_time = clock();
+    triFusion(table, n);
+    end_time = clock();
+    double elapsed_time_inverse_ordered = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
+    printf("Table Inverse Ordered (Sorted): ");
+    printf("\n");
+    printf("Time of execution: %f seconds\n", elapsed_time_inverse_ordered);
+    free(table);
 
 
 
     return 0;
-
-
-
-
 }
